cb651bb3208127ab1edc4b990cc9c2c420d1fd37
kikunantoka <kikunantoka@gmail.com>
2014-06-11 03:17:36 +0900

    Cofirm value

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,10 +28,12 @@ class SessionsController < ApplicationController
 
     res = @access_token.get('https://api.zaim.net/v2/home/user/verify')
     json = JSON.parse(res.body)
+    result = JSON.pretty_generate(json)
     session[:name] = json['me']['name']
     session[:oauth_token] = session[:oauth_token_secret] = nil
 
-    redirect_to root_path
+    render :text=>result
+    # redirect_to root_path
   end
 
   def callback
